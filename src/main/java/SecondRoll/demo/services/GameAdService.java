2f33b0ee9fef33bfd85c296b40653a999aa45bf1
Emma Agnell <jerker.agnell@gmail.com>
2024-02-19 10:04:24 +0100

    Basic crud gameads

---
+++ src/main/java/SecondRoll/demo/services/GameAdService.java
@@ -0,0 +1,41 @@
+package SecondRoll.demo.services;
+
+import SecondRoll.demo.models.GameAds;
+import SecondRoll.demo.repository.GameAdsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class GameAdService {
+    @Autowired
+    GameAdsRepository gameAdsRepository;
+    // Create a gameAd
+    public GameAds createGameAd(GameAds gameAds) {
+        return gameAdsRepository.save(gameAds);
+    }
+
+    // Get all gameAds
+    public List<GameAds> getAllGameAds() {
+        return gameAdsRepository.findAll();
+    }
+
+    // Update a book
+    public GameAds updateGameAd(GameAds gameAds) {
+        return gameAdsRepository.save(gameAds);
+    }
+
+    // Get a book by id
+
+    public Optional<GameAds> getGameAdById(String id) {
+        return gameAdsRepository.findById(id);
+    }
+
+    // Delete a book
+    public String deleteGameAd(String id) {
+        gameAdsRepository.deleteById(id);
+        return "Game Ad deleted";
+    }
+
+}
