7a02050774e7526c35ae6dae3c8cc13eda9e16eb
Mia Stefansdotter <stefansdotter@gmail.com>
2024-02-21 12:54:16 +0100

    working on WebSecurityConfig

--- src/main/java/SecondRoll/demo/Security/WebSecurityConfig.java
+++ src/main/java/SecondRoll/demo/Security/WebSecurityConfig.java
@@ -1,4 +1,61 @@
 package SecondRoll.demo.Security;
 
+import SecondRoll.demo.Security.jwt.AuthTokenFilter;
+import SecondRoll.demo.Security.jwt.AuthenticationEntryJwt;
+import SecondRoll.demo.Security.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private AuthenticationEntryJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+
+
+
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(authenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
