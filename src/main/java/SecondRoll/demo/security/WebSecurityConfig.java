65070875f09e329afb45420fe5444cb4c8c0ad8c
Vurpas <erikengstrom164@gmail.com>
2024-02-22 15:39:41 +0100

    changed endpaths in the WebSecurityConfig

--- src/main/java/SecondRoll/demo/security/WebSecurityConfig.java
+++ src/main/java/SecondRoll/demo/security/WebSecurityConfig.java
@@ -61,8 +61,8 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/**").permitAll()
-                                .requestMatchers("/api/test/**").permitAll()
-                                .requestMatchers("/api/test/user").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/api/users/**").permitAll()
+                                .requestMatchers("/api/users").hasAnyRole("USER", "ADMIN")
                                 .anyRequest().authenticated()
 
                 ).httpBasic(Customizer.withDefaults());
