fb19a9660f02b72a70f7db86c60aa25fd016af27
Mia Stefansdotter <stefansdotter@gmail.com>
2024-02-29 12:01:45 +0100

    working on game ad randomizer method in GameAdsService

--- src/main/java/SecondRoll/demo/controllers/GameAdsController.java
+++ src/main/java/SecondRoll/demo/controllers/GameAdsController.java
@@ -3,17 +3,14 @@ package SecondRoll.demo.controllers;
 import SecondRoll.demo.models.EGameCategory;
 import SecondRoll.demo.models.GameAds;
 import SecondRoll.demo.payload.CreateGameDTO;
-import SecondRoll.demo.repository.GameAdsRepository;
 import SecondRoll.demo.services.GameAdsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @RestController
 @RequestMapping(value="/api/gameAds")
@@ -68,16 +65,6 @@ public class GameAdsController {
         Optional<GameAds> gameAds = gameAdsService.getGameByUserId(userId);
         return gameAds.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    // Roll the Dice game ad randomizer
-    @GetMapping(value = "/randomizer")
-    public void List<GameAds> randomizeGameAds(GameAds gameAds) {
-        List<Integer> GameAds = Arrays.asList();
-        Random random = new Random();
-        int randomGameAd = GameAds.get(random.nextInt(GameAds.size()));
-
-       System.out.println("Random game ad " + randomGameAd);
     }
 
 }
