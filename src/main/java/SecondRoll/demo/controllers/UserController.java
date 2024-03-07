55162e7bf7911dba0f3527135934a1307941c346
Vurpas <erikengstrom164@gmail.com>
2024-03-07 09:44:59 +0100

    Created new custom messageResponse for adding a game to wishlist

--- src/main/java/SecondRoll/demo/controllers/UserController.java
+++ src/main/java/SecondRoll/demo/controllers/UserController.java
@@ -3,6 +3,7 @@ package SecondRoll.demo.controllers;
 import SecondRoll.demo.models.Rating;
 import SecondRoll.demo.models.User;
 import SecondRoll.demo.payload.WishlistDTO;
+import SecondRoll.demo.payload.response.MessageResponse;
 import SecondRoll.demo.payload.response.UserProfileResponse;
 import SecondRoll.demo.repository.UserRepository;
 import SecondRoll.demo.services.UserService;
@@ -57,11 +58,12 @@ public class UserController {
     @PutMapping("/{username}/wishlist")
     @PreAuthorize("#username == principal.username")
         public ResponseEntity<?> addGameToWishlist (@PathVariable("username") String username, @RequestBody WishlistDTO wishlistDTO){
-            User userWithWishList = userService.addGameToWishlist(username, wishlistDTO);
-            return new ResponseEntity<>(userWithWishList, HttpStatus.CREATED);
+            userService.addGameToWishlist(username, wishlistDTO);
+        return  ResponseEntity.ok().header(String.valueOf(HttpStatus.CREATED))
+                .body(new MessageResponse("Game added to wishlist"));
         }
 
-    // REMOVE a gameAd to a user wishlist using a Data Transfer Object-reference.
+    // REMOVE a gameAd from a user wishlist using a Data Transfer Object-reference.
     @DeleteMapping(value = "/{userId}/wishlist")
     public ResponseEntity<?> removeGameFromWishlist(@PathVariable String userId, @RequestBody WishlistDTO wishlistDTO) {
         User userWithWishList = userService.removeGameFromWishlist(userId, wishlistDTO);
