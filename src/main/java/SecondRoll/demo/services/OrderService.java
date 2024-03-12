8095b3ed2da862d008518308a12414a8f9eaf60a
Teis Loyche <teisloyche@hotmail.com>
2024-03-12 14:06:06 +0100

    Finished catching Exceptions for order- and user-methods with corresponding messages in postman

--- src/main/java/SecondRoll/demo/services/OrderService.java
+++ src/main/java/SecondRoll/demo/services/OrderService.java
@@ -113,9 +113,6 @@ public class OrderService {
     public List<BuyerHistoryResponse> buyerOrderHistory(String buyerId) {
 
         List<Order> orders = orderRepository.findByBuyerId(buyerId);
-        /* if (!buyerId.isEmpty()) {
-            throw new ServiceException("Buyer with id: " + buyerId + " was not found.");
-        } */
 
         return orders.stream().map(this::convertToBuyerHistoryDTO).collect(Collectors.toList());
     }
