a35746083f07ef3edf912396c020c6bcece83035
Teis Loyche <teisloyche@hotmail.com>
2024-09-12 09:45:33 +0200

    test commit for branch within a branch

--- src/main/java/SecondRoll/demo/controllers/CategoryController.java
+++ src/main/java/SecondRoll/demo/controllers/CategoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value="/api/gameAds")
+@RequestMapping(value="/api/gameads")
 public class CategoryController {
 
     @Autowired
